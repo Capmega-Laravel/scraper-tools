9b5f4f7abb170913b697f45fcc2aaabc149a1f3a
Camilo Rodriguez <camilo@sdkconsultoria.com>
2020-04-22 17:16:25 -0500

    Implemenet clear data

--- src/Models/ScrapingData.php
+++ src/Models/ScrapingData.php
@@ -3,9 +3,12 @@
 namespace Sdkconsultoria\BlogScraping\Models;
 
 use Sdkconsultoria\Base\Models\ResourceModel;
+use Symfony\Component\DomCrawler\Crawler;
 
 class ScrapingData extends ResourceModel
 {
+    private $html;
+
     /**
      * Get the validation rules that apply to the request.
      *
@@ -44,5 +47,76 @@ class ScrapingData extends ResourceModel
     public function images()
     {
         return $this->hasMany('Sdkconsultoria\BlogScraping\Models\ScrapingDataImage', 'scraping_data_id', 'id');
+    }
+
+    public function getData()
+    {
+        $this->html = [];
+
+        $crawler = new Crawler($this->description);
+        $crawler = $crawler->filter('div')->first();
+
+        foreach ($crawler->children() as $domElement) {
+            $this->html[] = [
+                'type'       => $domElement->nodeName,
+                'attributes' => $this->parseAttr($domElement->attributes??[]),
+                'childs'     => $this->parse($domElement->childNodes),
+                'text'       => $this->parseText($domElement),
+            ];
+        }
+
+        dd($this->html);
+    }
+
+    protected function parse($elements)
+    {
+        $html = [];
+
+        foreach ($elements as $domElement) {
+            if ($domElement->nodeName != '#text') {
+                $html[] = [
+                    'type'       => $domElement->nodeName,
+                    'attributes' => $this->parseAttr($domElement->attributes??[]),
+                    'childs'     => $this->parse($domElement->childNodes),
+                    'text'       => $this->parseText($domElement),
+                ];
+            }
+        }
+
+        return $html;
+    }
+
+    private function parseAttr($attributes)
+    {
+        $attributes_array = [];
+
+        foreach ($attributes as $key => $attribute) {
+            $attributes_array[] = [
+                'name'   => $attribute->nodeName,
+                'value'  => $attribute->nodeValue,
+            ];
+        }
+
+        return $attributes_array;
+    }
+
+    private function parseText($domElement)
+    {
+        if ($domElement->nodeName == 'p' ||
+            $domElement->nodeName == 'span' ||
+            $domElement->nodeName == 'label' ||
+            $domElement->nodeName == 'em' ||
+            $domElement->nodeName == 'mark' ||
+            $domElement->nodeName == 'i' ||
+            $domElement->nodeName == 'b' ||
+            $domElement->nodeName == 'u' ||
+            $domElement->nodeName == 's' ||
+            $domElement->nodeName == 'cite' ||
+            $domElement->nodeName == 'small' ||
+            $domElement->nodeName == 'strong') {
+            return $domElement->textContent;
+        }
+
+        return '';
     }
 }
